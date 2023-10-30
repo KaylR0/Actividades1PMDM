c76f79a2a5cea18c76688330eae8b2e3e13e6b47
Alumno <ifclosenlaces@gmail.com>
2023-10-30 19:29:25 +0100

    Iniciadas clases para app colorPalette

---
+++ app/src/main/java/com/kaylr/chat/colorPalette/colorPalette.kt
@@ -0,0 +1,12 @@
+package colorPalette
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kaylr.chat.R
+
+class colorPalette : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_color_palette)
+    }
+}
